7ab5a76fd551922aed2d92b29b4283a1599c6e97
Begüm Özkaya <boe1301@students.fhv.at>
2026-03-23 20:08:27 +0100

    Exercise 3

---
+++ order-service/src/test/java/at/fhv/orderservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
